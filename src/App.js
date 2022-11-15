0e3ea6ccf4ca0cba110432daa5f75a59599027d8
Min Hyung Lee <tm9704@korea.ac.kr>
2022-11-15 22:53:02 +0900

    Add UserList Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 const App = () => {
   return (
     <div>
       <AddUser />
+      <UserList users={[]} />
     </div>
   );
 };
